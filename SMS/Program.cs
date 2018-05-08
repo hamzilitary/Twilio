6efb1ab4314f2f3f21498ada6a458939bdd115d4
Joel Adams <joeladamsdesign@gmail.com>
2018-05-08 14:27:48 -0700

    transform JSON to C# Object

--- SMS/Program.cs
+++ SMS/Program.cs
@@ -4,9 +4,17 @@ using RestSharp;
 using RestSharp.Authenticators;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace SMS
 {
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -24,7 +32,14 @@ namespace SMS
             }).Wait();
             //4
             JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
-            Console.WriteLine(jsonResponse["messages"]);
+            List<Message> messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+            foreach (Message message in messageList)
+            {
+                Console.WriteLine("To: {0}", message.To);
+                Console.WriteLine("From: {0}", message.From);
+                Console.WriteLine("Body: {0}", message.Body);
+                Console.WriteLine("Status: {0}", message.Status);
+            }
             Console.ReadLine();
         }
 

6fc64dbb00614a9e86ff8d7edd6c4bddc18305ef
Joel Adams <joeladamsdesign@gmail.com>
2018-05-08 14:14:43 -0700

    add request for api and show parsed data

--- SMS/Program.cs
+++ SMS/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace SMS
 {
@@ -6,7 +11,32 @@ namespace SMS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            //1
+            var request = new RestRequest("Accounts/AC8b217d4ea2df3496bb72270f476b04bd/Messages.json", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator("AC8b217d4ea2df3496bb72270f476b04bd", "029b77f8e0576603d95beb020a22a87d");
+            //2
+            var response = new RestResponse();
+            //3a
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            //4
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            Console.WriteLine(jsonResponse["messages"]);
+            Console.ReadLine();
+        }
+
+        //3b
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
         }
     }
 }

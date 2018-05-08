59162a15981e6802aeddbd5d8d4c9de00447a7f2
Joel Adams <joeladamsdesign@gmail.com>
2018-05-08 12:11:46 -0700

    Add project files.

---
+++ SMS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
